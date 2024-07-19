48505629f6796dc004265c2bd14cb0d75489d994
Tainá Dreissig <88323837+TainaDr@users.noreply.github.com>
2024-07-19 11:28:36 -0300

    Add files via upload

---
+++ src/components/ImagensDiversas.vue
@@ -0,0 +1,28 @@
+<template>
+      <div id="imagemGrande">
+            <img src="@/assets/pexels-eberhard-grossgasteiger-691668 (1).jpg" alt="Imagem 1" width="100%">
+      </div>
+      <br>
+      <article class="flexbox">
+            <img src="@/assets/pexels-mohamed-almari-1485894.jpg" alt="Imagem 2" width="410" height="300">
+            <img src="@/assets/pexels-pok-rie-2049422.jpg" alt="Imagem 3" width="410" height="300">
+            <img src="@/assets/images (9).jpeg" alt="Imagem 3" width="410" height="300">
+      </article>
+      <br>
+</template>
+
+<style>
+#imagemGrande {
+      display: flex;
+      flex-wrap: nowrap;
+      justify-content: center;
+}
+
+
+.flexbox {
+      display: flex;
+      justify-content: center;
+      flex-wrap: wrap;
+      align-items: center;
+}
+</style>
---
+++ src/components/PaginaPrincipal.vue
@@ -0,0 +1,37 @@
+<template>
+    <OutroComponente />
+    <br>
+    <ImagensDiversas />
+    <br>
+    <VideoUnico />
+    <br>
+    <TabelaDiversos />
+    <footer></footer>
+</template>
+<script>
+
+import OutroComponente from './OutroComponente.vue'
+import ImagensDiversas from './ImagensDiversas.vue'
+import VideoUnico from './VideoUnico.vue'
+import TabelaDiversos from './TabelaDiversos.vue'
+
+export default {
+    name: 'PaginaPrincipal',
+    components: {
+        ImagensDiversas, VideoUnico, OutroComponente, TabelaDiversos
+    }
+}
+
+</script>
+
+<style>
+body {
+    background-color: rgb(255, 255, 255);
+}
+
+footer {
+    background-color: rgb(0, 38, 76);
+    height: 20px;
+    margin: 0;
+}
+</style>
